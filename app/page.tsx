ec14c2170a5264dee39dd26fe96bf1f4d106971d
pinkjagnie <olga.krzysztofik@gmail.com>
2023-11-20 16:29:30 +0100

    Fixed styling issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-24 bg-zinc-100 text-slate-900">
-      <h1>Astro users</h1>
+    <main className="min-h-screen bg-zinc-100 text-slate-900">
+      <Hero />
     </main>
   );
 }
